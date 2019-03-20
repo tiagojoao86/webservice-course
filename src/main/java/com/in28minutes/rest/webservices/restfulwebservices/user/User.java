bb6165232c11a4df0ec2317a59abed40b39d68f3
tiagojoao86 <pereira.tiagojoao@gmail.com>
2019-03-20 00:25:01 -0300

    20032019

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -8,6 +8,10 @@ public class User {
 	private String name;
 	private Date birthDate;
 	
+	public User() {
+		
+	}
+	
 	
 	public User(Integer id, String name, Date birthDate) {
 		super();
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<User>();
